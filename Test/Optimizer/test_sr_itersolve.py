22f06136c5039df6a99970c42d086c471196f774
Filippo Vicentini <filippovicentini@gmail.com>
2021-05-11 12:39:10 +0200

    fix_

--- Test/Optimizer/test_sr_itersolve.py
+++ Test/Optimizer/test_sr_itersolve.py
@@ -1,19 +1,22 @@
 import pytest
 
-import jax
-import jax.numpy as jnp
-import jax.flatten_util
-import numpy as np
-from jax.scipy.sparse.linalg import cg
-from netket.optimizer.qgt import qgt_onthefly_logic as _sr_onthefly_logic
-from functools import partial
 import itertools
+from functools import partial
+
+
+import flax
+import jax
+
+import numpy as np
 from numpy import testing
 
-import jax
+import jax.numpy as jnp
+import jax.flatten_util
+from jax.scipy.sparse.linalg import cg
 
-from netket.optimizer import qgt
 import netket as nk
+from netket.optimizer import qgt
+from netket.optimizer.qgt import qgt_onthefly_logic as _sr_onthefly_logic
 
 from .. import common
 
@@ -46,6 +49,9 @@ def vstate(request):
     vstate = nk.variational.MCState(
         nk.sampler.MetropolisLocal(hi),
         ma,
+    )
+    vstate.init_parameters(
+        nk.nn.initializers.normal(stddev=0.001), seed=jax.random.PRNGKey(3)
     )
 
     vstate.sample()
