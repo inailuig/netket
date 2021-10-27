499beda9bd7f124f69fd50b42490a69cf511dede
Clemens Giuliani <clemens@inailuig.it>
2021-10-27 09:43:43 +0200

    tests

--- test/optimizer/test_qgt_itersolve.py
+++ test/optimizer/test_qgt_itersolve.py
@@ -32,6 +32,10 @@ QGT_objects = {}
 
 QGT_objects["OnTheFly"] = partial(qgt.QGTOnTheFly, diag_shift=0.01)
 
+QGT_objects["OnTheFly(chunk_size=16)"] = partial(
+    qgt.QGTOnTheFly, diag_shift=0.01, chunk_size=16
+)
+
 QGT_objects["JacobianPyTree"] = partial(qgt.QGTJacobianPyTree, diag_shift=0.01)
 QGT_objects["JacobianPyTree(mode=holomorphic)"] = partial(
     qgt.QGTJacobianPyTree, holomorphic=True, diag_shift=0.01
