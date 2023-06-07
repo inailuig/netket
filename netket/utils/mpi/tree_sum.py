5171d35b1c055374ca61bf84c156bf65004acdd6
Clemens Giuliani <clemens@inailuig.it>
2023-06-07 19:33:32 +0200

    switch to a custom primitive

--- netket/utils/mpi/tree_sum.py
+++ netket/utils/mpi/tree_sum.py
@@ -1,28 +1,53 @@
-# transposable version of mpi_tree_map(mpi.mpi_sum_jax, ...)
 import jax
-from jax import custom_derivatives
-from . import mpi_tree_map, mpi_sum_jax
+import jax.numpy as jnp
+from functools import partial
+from . import mpi_sum_jax
 
-class _custom_transpose:
-    def __init__(self, out_types, fun):
-        from jax.custom_transpose import custom_transpose
 
-        self.out_types = out_types
-        self.fun = custom_transpose(fun)
-
-    def __getattr__(self, name):
-        return getattr(self.fun, name)
-
-    def __call__(self, *args):
-        return self.fun(self.out_types, *args)
-
-def _mpi_tree_sum(_, x):
+def _mpi_args_sum(*args):
     token = jax.lax.create_token()
-    res, token = mpi_tree_map(mpi_sum_jax, x, token=token)
-    return res
+    res = []
+    for x in args:
+        y, token = mpi_sum_jax(x, token=token)
+        res.append(y)
+    return tuple(res)
 
-def _mpi_tree_sum_transposed(_, x):
-    return x
+
+def _mpi_args_sum_impl(*args, transpose):
+    if transpose:
+        return args
+    else:
+        return _mpi_args_sum(*args)
+
+
+def _mpi_args_sum_transpose_rule(tan_args, *x_args, transpose):
+    return _mpi_args_sum_impl(*tan_args, transpose=not transpose)
+
+
+def _mpi_args_sum_abstract_eval(*args, transpose):
+    return map(lambda x: jax.abstract_arrays.ShapedArray(x.shape, x.dtype), args)
+
+
+def _batch_rule(prim, batched_args, batch_dims, **params):
+    return prim.bind(*batched_args, **params), batch_dims
+
+
+mpi_args_sum_p = jax.core.Primitive("mpi_args_sum")
+mpi_args_sum_p.multiple_results = True
+mpi_args_sum_p.def_impl(_mpi_args_sum_impl)
+mpi_args_sum_p.def_abstract_eval(_mpi_args_sum_abstract_eval)
+jax.interpreters.ad.primitive_transposes[mpi_args_sum_p] = _mpi_args_sum_transpose_rule
+jax.interpreters.xla.register_initial_style_primitive(mpi_args_sum_p)
+jax.interpreters.mlir.register_lowering(
+    mpi_args_sum_p,
+    jax.interpreters.mlir.lower_fun(_mpi_args_sum_impl, multiple_results=True),
+)
+jax.interpreters.batching.primitive_batchers[mpi_args_sum_p] = partial(
+    _batch_rule, mpi_args_sum_p
+)
+
 
 def mpi_tree_sum(x):
-    return custom_derivatives.linear_call(_mpi_tree_allreduce_sum, _mpi_tree_allreduce_sum_transposed, (), x)
+    leaves, treedef = jax.tree_util.tree_flatten(x)
+    out = mpi_args_sum_p.bind(*leaves, transpose=False)
+    return treedef.unflatten(out)
