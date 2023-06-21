3abf90c27fa379fe5ebe3e1b60964793bdc6ad7b
Clemens Giuliani <clemens@inailuig.it>
2023-06-21 11:24:21 +0200

    extract replicated for stats repr

--- netket/stats/mc_stats.py
+++ netket/stats/mc_stats.py
@@ -52,6 +52,12 @@ def _maybe_item(x):
     else:
         return x
 
+def _extract_replicated(x):
+    if isinstance(x, jax.Array) and not x.is_fully_addressable:
+        assert x.is_fully_replicated
+        return x.addressable_data(0)
+    else:
+        return x
 
 @struct.dataclass
 class Stats:
@@ -108,14 +114,15 @@ class Stats:
         return "Mean", self.to_dict()
 
     def __repr__(self):
-        mean, err, var = _format_decimal(self.mean, self.error_of_mean, self.variance)
-        if not math.isnan(self.R_hat):
-            ext = f", R̂={self.R_hat:.4f}"
+        s = jax.tree_map(_extract_replicated, self)
+        mean, err, var = _format_decimal(s.mean, s.error_of_mean, s.variance)
+        if not math.isnan(s.R_hat):
+            ext = f", R̂={s.R_hat:.4f}"
         else:
             ext = ""
         if config.netket_experimental_fft_autocorrelation:
-            if not (math.isnan(self.tau_corr) and math.isnan(self.tau_corr_max)):
-                ext += f", τ={self.tau_corr:.1f}<{self.tau_corr_max:.1f}"
+            if not (math.isnan(s.tau_corr) and math.isnan(s.tau_corr_max)):
+                ext += f", τ={s.tau_corr:.1f}<{s.tau_corr_max:.1f}"
         return f"{mean} ± {err} [σ²={var}{ext}]"
 
     # Alias accessors
