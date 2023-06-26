1f0b227e46fb5aceb5db91f0f281d5eb4a399f39
Clemens Giuliani <clemens@inailuig.it>
2023-06-26 16:28:51 +0200

    wrap numba op with callback, such that output has sharding as expected; uses one call per device array

--- netket/vqs/mc/mc_state/expect.py
+++ netket/vqs/mc/mc_state/expect.py
@@ -21,6 +21,7 @@ from jax import numpy as jnp
 from netket.stats import Stats, statistics as mpi_statistics
 from netket.utils.types import PyTree
 from netket.utils.dispatch import dispatch
+from netket.jax import replicate_sharding
 
 from netket.operator import (
     AbstractOperator,
@@ -45,7 +46,7 @@ def get_local_kernel_arguments(vstate: MCState, Ô: Squared):  # noqa: F811
     check_hilbert(vstate.hilbert, Ô.hilbert)
 
     σ = vstate.samples
-    σp, mels = Ô.parent.get_conn_padded(σ)
+    σp, mels = replicate_sharding(Ô.parent.get_conn_padded)(σ)
     return σ, (σp, mels)
 
 
@@ -59,7 +60,7 @@ def get_local_kernel_arguments(vstate: MCState, Ô: DiscreteOperator):  # noqa:
     check_hilbert(vstate.hilbert, Ô.hilbert)
 
     σ = vstate.samples
-    σp, mels = Ô.get_conn_padded(σ)
+    σp, mels = replicate_sharding(Ô.get_conn_padded)(σ)
     return σ, (σp, mels)
 
 
