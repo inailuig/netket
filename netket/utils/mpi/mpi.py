fa637875aaa595c00336c45b8643c80b8a3022da
Clemens Giuliani <clemens@inailuig.it>
2023-06-28 15:20:59 +0200

    disable mpi

--- netket/utils/mpi/mpi.py
+++ netket/utils/mpi/mpi.py
@@ -24,7 +24,9 @@ _mpi4jax_loaded = False
 mpi4jax_available = False
 
 try:
-    if not config.netket_mpi:  # pragma: no cover
+    if (
+        not config.netket_mpi or netket.config.netket_experimental_pjit
+    ):  # pragma: no cover
         # if mpi is disabled trigger import error
         # and follow the no-mpi code path
         raise ImportError
