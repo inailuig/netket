07985c81c90eb4a250319b18badd1761b96b8fe0
Clemens Giuliani <clemens@inailuig.it>
2023-08-24 15:55:52 +0200

    same for otf

--- netket/optimizer/qgt/qgt_onthefly_logic.py
+++ netket/optimizer/qgt/qgt_onthefly_logic.py
@@ -22,8 +22,13 @@ from netket.jax import (
     scanmap,
     scan_reduce,
     scan_append,
-    unchunk,
+    chunk,
 )
+
+from netket.utils import config
+
+from jax.experimental.shard_map import shard_map
+from jax.sharding import Mesh, PartitionSpec as P
 
 # Stochastic Reconfiguration with jvp and vjp
 
@@ -93,7 +98,7 @@ def mat_vec_factory(forward_fn, params, model_state, samples, pdf=None):
 
 
 @partial(scanmap, scan_fun=scan_append, argnums=2)
-def _O_jvp(forward_fn, params, samples, v):
+def __O_jvp(forward_fn, params, samples, v):
     # TODO apply the transpose of sum_inplace (allreduce) to the arg v here
     # in order to get correct transposition with MPI
     _, res = jax.jvp(lambda p: forward_fn(p, samples), (params,), (v,))
@@ -101,58 +106,93 @@ def _O_jvp(forward_fn, params, samples, v):
 
 
 @partial(scanmap, scan_fun=scan_reduce, argnums=(2, 3))
-def _O_vjp(forward_fn, params, samples, w):
+def __O_vjp(forward_fn, params, samples, w):
     _, vjp_fun = jax.vjp(forward_fn, params, samples)
     res, _ = vjp_fun(w)
     return res
 
 
-def _OH_w(forward_fn, params, samples, w):
-    return tree_conj(_O_vjp(forward_fn, params, samples, w.conjugate()))
+# TODO shard_map only if config.netket_experimental_pjit
 
 
-def _Odagger_DeltaO_v(forward_fn, params, samples, v, pdf=None):
-    w = _O_jvp(forward_fn, params, samples, v)
+def _O_jvp(forward_fn, params, samples, v, chunk_size):
+    mesh = Mesh(jax.devices(), axis_names=("i"))
+    in_specs = P(), P("i"), P()
+    out_specs = P("i")
+
+    @partial(shard_map, mesh=mesh, in_specs=in_specs, out_specs=out_specs)
+    def _jvp(params, samples, v):
+        samples, ucf = chunk(samples, chunk_size)
+        res = __O_jvp(forward_fn, params, samples, v)
+        return ucf(res)
+
+    return _jvp(params, samples, v)
+
+
+def _O_vjp(forward_fn, params, samples, w, chunk_size):
+    mesh = Mesh(jax.devices(), axis_names=("i"))
+    in_specs = P(), P("i"), P("i")
+    out_specs = P()
+
+    @partial(shard_map, mesh=mesh, in_specs=in_specs, out_specs=out_specs)
+    def _vjp(params, samples, w):
+        samples, _ = chunk(samples, chunk_size)
+        w, _ = chunk(w, chunk_size)
+        res = __O_vjp(forward_fn, params, samples, w)
+        return jax.tree_map(partial(jax.lax.psum, axis_name="i"), res)
+
+    return _vjp(params, samples, w)
+
+
+def _OH_w(forward_fn, params, samples, w, chunk_size):
+    return tree_conj(_O_vjp(forward_fn, params, samples, w.conjugate(), chunk_size))
+
+
+def _Odagger_DeltaO_v(forward_fn, params, samples, v, chunk_size, pdf=None):
+    w = _O_jvp(forward_fn, params, samples, v, chunk_size)
     if pdf is None:
-        w = w * (1.0 / (samples.shape[0] * samples.shape[1] * mpi.n_nodes))
-        w_, chunk_fn = unchunk(w)
-        w = chunk_fn(subtract_mean(w_))  # w/ MPI
+        w = w * (1.0 / (samples.shape[0] * mpi.n_nodes))
+        w = subtract_mean(w)  # w/ MPI
     else:
-        w_, chunk_fn = unchunk(w)
-        # here we assume pdf is chunked,
-        # which we undo as the flat vector is needed here
-        pdf_, _ = unchunk(pdf)
-        w_ = pdf_ * (w_ - mpi.mpi_sum_jax(pdf_ @ w_)[0])
-        w = chunk_fn(w_)
-    res = _OH_w(forward_fn, params, samples, w)
+        w = pdf_ * (w - mpi.mpi_sum_jax(pdf @ w)[0])
+    res = _OH_w(forward_fn, params, samples, w, chunk_size)
     return jax.tree_map(lambda x: mpi.mpi_sum_jax(x)[0], res)  # MPI
 
 
 # @partial(jax.jit, static_argnums=1)
-def _mat_vec_chunked(forward_fn, params, samples, v, diag_shift, pdf=None):
-    res = _Odagger_DeltaO_v(forward_fn, params, samples, v, pdf)
+def _mat_vec_chunked(forward_fn, params, samples, v, diag_shift, chunk_size, pdf=None):
+    assert samples.ndim == 2  # require flat samples, axis 0 can be sharded
+    res = _Odagger_DeltaO_v(forward_fn, params, samples, v, chunk_size, pdf)
     return tree_axpy(diag_shift, v, res)
 
 
-def _mat_vec_chunked_transposable(forward_fn, params, samples, v, diag_shift, pdf=None):
+def _mat_vec_chunked_transposable(
+    forward_fn, params, samples, v, diag_shift, chunk_size, pdf=None
+):
     extra_args = (params, samples, diag_shift, pdf)
 
     def _mv(extra_args, x):
         params, samples, diag_shift, pdf = extra_args
-        return _mat_vec_chunked(forward_fn, params, samples, x, diag_shift, pdf)
+        return _mat_vec_chunked(
+            forward_fn, params, samples, x, diag_shift, chunk_size, pdf
+        )
 
     def _mv_trans(extra_args, y):
         # the linear operator is hermitian
         params, samples, diag_shift, pdf = extra_args
         return tree_conj(
-            _mat_vec_chunked(forward_fn, params, samples, tree_conj(y), diag_shift, pdf)
+            _mat_vec_chunked(
+                forward_fn, params, samples, tree_conj(y), diag_shift, chunk_size, pdf
+            )
         )
 
     return jax.custom_derivatives.linear_call(_mv, _mv_trans, extra_args, v)
 
 
-@partial(jax.jit, static_argnums=0)
-def mat_vec_chunked_factory(forward_fn, params, model_state, samples, pdf=None):
+@partial(jax.jit, static_argnums=(0, 5))
+def mat_vec_chunked_factory(
+    forward_fn, params, model_state, samples, pdf=None, chunk_size=None
+):
     """
 
     Prepare a function which computes the regularized SR matrix-vector product
@@ -184,6 +224,9 @@ def mat_vec_chunked_factory(forward_fn, params, model_state, samples, pdf=None):
         return forward_fn({"params": W, **model_state}, samples)
 
     return Partial(
-        partial(_mat_vec_chunked_transposable, fun), params, samples, pdf=pdf
+        partial(_mat_vec_chunked_transposable, fun, chunk_size=chunk_size),
+        params,
+        samples,
+        pdf=pdf,
     )
     # return Partial(lambda f, *args: jax.jit(f)(*args), Partial(partial(_mat_vec_chunked, fun), params, samples))
