6a2a7e3ad9eb7de868a9564fca7e75107bc43f6d
Clemens Giuliani <clemens@inailuig.it>
2023-10-27 14:51:45 +0200

    u another qgt test

--- test/optimizer/test_qgt_logic.py
+++ test/optimizer/test_qgt_logic.py
@@ -261,7 +261,7 @@ def test_reassemble_complex(e):
 
 
 @common.named_parametrize("holomorphic", [True, False])
-@common.named_parametrize("n_samp", [24, 1024])
+@common.named_parametrize("n_samp", [48, 1024])
 @common.named_parametrize("jit", [True, False])
 @pytest.mark.parametrize("outdtype, pardtype", all_test_types)
 @common.named_parametrize("chunk_size", [8, None])
