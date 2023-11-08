f7e853b37ded4e3bf5f66b429d4fb77feb4ba730
Clemens Giuliani <clemens@inailuig.it>
2023-11-08 12:06:45 +0100

    Update run_test_sharding_distributed.sh

--- test_sharding/run_test_sharding_distributed.sh
+++ test_sharding/run_test_sharding_distributed.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mpirun --hostfile /etc/hostfile -x NETKET_EXPERIMENTAL_SHARDING=1 -x NETKET_MPI_WARNING=0 pytest-3 -p no:warnings --color=yes --verbose -n 0 --tb=short test_sharding_distributed.py
+mpirun --hostfile /etc/hostfile -x NETKET_EXPERIMENTAL_SHARDING=1 -x NETKET_MPI_WARNING=0 pytest-3 -p no:warnings --color=yes --verbose -n 0 --tb=short test_sharding/test_sharding_distributed.py
