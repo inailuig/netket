da8069fc46318b2a672955ac8a41d30531f57a7a
Clemens Giuliani <clemens@inailuig.it>
2024-04-23 11:49:46 +0200

    f

--- netket/hilbert/random/base.py
+++ netket/hilbert/random/base.py
@@ -129,7 +129,8 @@ def flip_state_scalar(hilb, key, state, indx):
 @dispatch
 @partial(sharding_decorator, sharded_args_tree=(False, "key", True, True))
 def flip_state_batch(hilb, key, states, indxs):
+    keys = jax.random.split(key, states.shape[0])
     res = jax.vmap(flip_state_scalar, in_axes=(None, 0, 0, 0), out_axes=0)(
-        hilb, key, states, indxs
+        hilb, keys, states, indxs
     )
     return res
