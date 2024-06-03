7c7ee8ca69b088474daf58515432870fcb38a43a
Clemens Giuliani <clemens@inailuig.it>
2024-06-03 10:36:18 +0200

    experiment

--- netket/jax/_expect.py
+++ netket/jax/_expect.py
@@ -20,10 +20,12 @@ from functools import partial
 import jax
 from jax import numpy as jnp
 
-from netket.stats import statistics as mpi_statistics, mean as mpi_mean, Stats
+from netket.stats import statistics as mpi_statistics, sum as mpi_sum, Stats
+from netket.utils import mpi
 from netket.utils.types import PyTree
 
-from ._vjp import vjp as nkvjp
+from ._vjp_chunked import vjp_chunked
+from ._vmap_chunked import apply_chunked
 
 
 def expect(
@@ -33,6 +35,7 @@ def expect(
     σ: jnp.ndarray,
     *expected_fun_args,
     n_chains: Optional[int] = None,
+    chunk_size: Optional[int] = None,
 ) -> tuple[jnp.ndarray, Stats]:
     """
     Computes the expectation value over a log-pdf.
@@ -41,14 +44,16 @@ def expect(
         log_pdf:
         expected_ffun
     """
-    return _expect(n_chains, log_pdf, expected_fun, pars, σ, *expected_fun_args)
+    return _expect(
+        n_chains, chunk_size, log_pdf, expected_fun, pars, σ, *expected_fun_args
+    )
 
 
 # log_prob_args and integrand_args are independent of params when taking the
 # gradient. They can be continuous or discrete, and they can be pytrees
 # Does not support higher-order derivatives yet
-@partial(jax.custom_vjp, nondiff_argnums=(0, 1, 2))
-def _expect(n_chains, log_pdf, expected_fun, pars, σ, *expected_fun_args):
+@partial(jax.custom_vjp, nondiff_argnums=(0, 1, 2, 3))
+def _expect(n_chains, chunk_size, log_pdf, expected_fun, pars, σ, *expected_fun_args):
     L_σ = expected_fun(pars, σ, *expected_fun_args)
     if n_chains is not None:
         L_σ = L_σ.reshape((n_chains, -1))
@@ -59,8 +64,10 @@ def _expect(n_chains, log_pdf, expected_fun, pars, σ, *expected_fun_args):
     return L̄_σ.mean, L̄_σ
 
 
-def _expect_fwd(n_chains, log_pdf, expected_fun, pars, σ, *expected_fun_args):
-    L_σ = expected_fun(pars, σ, *expected_fun_args)
+def _expect_fwd(
+    n_chains, chunk_size, log_pdf, expected_fun, pars, σ, *expected_fun_args
+):
+    L_σ = apply_chunked(lambda σ: expected_fun(pars, σ, *expected_fun_args))
     if n_chains is not None:
         L_σ_r = L_σ.reshape((n_chains, -1))
     else:
@@ -81,19 +88,30 @@ def _expect_fwd(n_chains, log_pdf, expected_fun, pars, σ, *expected_fun_args):
 # so it should support higher-order derivatives
 # But I don't know how to transform log_prob_fun into grad(log_prob_fun) while
 # keeping the chunk dimension and without a loop through the chunk dimension
-def _expect_bwd(n_chains, log_pdf, expected_fun, residuals, dout):
+def _expect_bwd(n_chains, chunk_size, log_pdf, expected_fun, residuals, dout):
     pars, σ, cost_args, ΔL_σ = residuals
     dL̄, dL̄_stats = dout
 
-    def f(pars, σ, *cost_args):
+    n_samples = σ.shape[0] * mpi.n_nodes
+
+    def f(pars, σ, ΔL_σ, *cost_args):
         log_p = log_pdf(pars, σ)
         term1 = jax.vmap(jnp.multiply)(ΔL_σ, log_p)
         term2 = expected_fun(pars, σ, *cost_args)
-        out = mpi_mean(term1 + term2, axis=0)
-        out = out.sum()
+        out = mpi_sum(term1 + term2, axis=0)
+        out = out.sum() / n_samples
         return out
 
-    _, pb = nkvjp(f, pars, σ, *cost_args)
+    pb = vjp_chunked(
+        f,
+        pars,
+        σ,
+        ΔL_σ,
+        *cost_args,
+        chunk_argnums=(1, 2),
+        chunk_size=chunk_size,
+        nondiff_argnums=(1, 2),
+    )
     grad_f = pb(dL̄)
     return grad_f
 
