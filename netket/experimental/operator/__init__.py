88d5b5b309ab14192cfd0f66112c1c5b1670ff19
Clemens Giuliani <clemens@inailuig.it>
2024-08-26 11:45:06 +0200

    add pnc spin

--- netket/experimental/operator/__init__.py
+++ netket/experimental/operator/__init__.py
@@ -19,6 +19,7 @@ from .pyscf import from_pyscf_molecule
 from ._particle_number_conserving_fermionic import (
     ParticleNumberConservingFermioperator2ndJax,
 )
+from ._particle_number_conserving_fermionic_spin import ParticleNumberConservingFermioperator2ndSpinJax
 
 
 from . import fermion
