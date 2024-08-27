569cb80612cc09d8606c05165376d1b5dfd2552b
Clemens Giuliani <clemens@inailuig.it>
2024-08-27 17:40:22 +0200

    update the test

--- test/operator/test_pnc.py
+++ test/operator/test_pnc.py
@@ -1,9 +1,9 @@
 import numpy as np
 import jax
 import jax.numpy as jnp
-from netket.experimental.operator import ParticleNumberConservingFermioperator2ndJax, FermionOperator2nd
-from netket.experimental.operator import ParticleNumberConservingFermioperator2ndSpinJax
+from netket.experimental.operator import ParticleNumberConservingFermioperator2ndJax, ParticleNumberConservingFermioperator2ndSpinJax, FermionOperator2nd
 from netket.experimental.hilbert import SpinOrbitalFermions
+from functools import partial
 
 import pytest
 
@@ -15,8 +15,8 @@ def _cast_normal_order(A):
     mask = (jnp.diff(idx_destroy) > 0).any(axis=1) | (jnp.diff(idx_create) > 0).any(axis=1)
     return A.at[idx[mask].T].set(0)
 
-
-def test_pnc():
+@pytest.mark.parametrize("desc", [True, False])
+def test_pnc(desc):
 
     N = 6
     n = 3
@@ -26,8 +26,11 @@ def test_pnc():
     k0, k1, k2, k3 = jax.random.split(jax.random.key(key), 4)
     c = jax.random.normal(k0)
     hij = jax.random.normal(k1, shape=(N,)*2)
-    hijkl = _cast_normal_order(jax.random.normal(k2, shape=(N,)*4))
-    hijklmn = _cast_normal_order(jax.random.normal(k3, shape=(N,)*6))
+    hijkl = jax.random.normal(k2, shape=(N,)*4)
+    hijklmn = jax.random.normal(k3, shape=(N,)*6)
+    if desc:
+        hijkl = _cast_normal_order(hijkl)
+        hijklmn = _cast_normal_order(hijklmn)
 
     hi = SpinOrbitalFermions(N, n_fermions=n)
 
@@ -45,15 +48,23 @@ def test_pnc():
     terms = terms + [f"{i}^ {j}^ {k}^ {l} {m} {n}" for i,j,k,l,m,n in list(zip(*ijklmn))]
     weights = weights + list(hijklmn[ijklmn])
     ha = FermionOperator2nd(hi, terms=terms, weights=weights)
-
-    ha2 = ParticleNumberConservingFermioperator2ndJax.from_sparse_arrays_normal_order(hi, [c, hij*(jnp.abs(hij) > cutoff), hijkl*(jnp.abs(hijkl) > cutoff), hijklmn*(jnp.abs(hijklmn) > cutoff)])
-
+    if desc:
+        factory = ParticleNumberConservingFermioperator2ndJax.from_sparse_arrays_normal_order
+    else:
+        factory = ParticleNumberConservingFermioperator2ndJax.from_sparse_arrays
+    ha2 = factory(hi, [c, hij*(jnp.abs(hij) > cutoff), hijkl*(jnp.abs(hijkl) > cutoff), hijklmn*(jnp.abs(hijklmn) > cutoff)])
     np.testing.assert_allclose(ha.to_dense(), ha2.to_dense())
 
+    ha3 = ParticleNumberConservingFermioperator2ndJax.from_fermiop(ha)
+    np.testing.assert_allclose(ha.to_dense(), ha3.to_dense())
 
-def test_pnc_spin():
-    N = 7
-    n = 3
+@pytest.mark.parametrize("N", [5])
+@pytest.mark.parametrize("n", [2,3])
+@pytest.mark.parametrize("s", [1/2, 1, 3/2])
+def test_pnc_spin(N, n, s):
+    # N = 7
+    # n = 3
+    # s = 1/2
     cutoff = 1e-4
     key = np.random.randint(2**32)
 
@@ -62,13 +73,16 @@ def test_pnc_spin():
     hij = jax.random.normal(k2, shape=(N,)*2)
     c = jax.random.normal(k3)
 
-    hi = SpinOrbitalFermions(N, s=1/2, n_fermions_per_spin=(n,n))
+    n_spin_subsectors = int(round(2*s+1))
+
+    hi = SpinOrbitalFermions(N, s=s, n_fermions_per_spin=(n,)*n_spin_subsectors)
 
     terms = []
     weights = []
     terms = terms + [""]
     weights = weights + [c]
-    idx_maps = [lambda i: i, lambda i: i+N] # spin down and up sectors
+    _f = lambda j,i: i+j*N
+    idx_maps = [partial(_f, j) for j in range(n_spin_subsectors)] # spin sectors
     for s in idx_maps:
         ij = jnp.where(jnp.abs(hij)>cutoff)
         terms = terms + [f"{s(i)}^ {s(j)}" for i,j in list(zip(*ij))]
@@ -80,6 +94,8 @@ def test_pnc_spin():
             weights = weights + list(hijkl[ijkl])
     ha = FermionOperator2nd(hi, terms=terms, weights=weights)
 
-    ha2 = ParticleNumberConservingFermioperator2ndSpinJax.from_sparse_arrays(hi, [c, hij*(jnp.abs(hij) > cutoff), hijkl*(jnp.abs(hijkl) > cutoff)])
-
+    ha2 = ParticleNumberConservingFermioperator2ndSpinJax.from_sparse_arrays_all_sectors(hi, [c, hij*(jnp.abs(hij) > cutoff), hijkl*(jnp.abs(hijkl) > cutoff)])
     np.testing.assert_allclose(ha.to_dense(), ha2.to_dense())
+
+    ha3 = ParticleNumberConservingFermioperator2ndSpinJax.from_fermiop(ha)
+    np.testing.assert_allclose(ha.to_dense(), ha3.to_dense())
