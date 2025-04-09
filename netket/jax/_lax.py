6292a639b0dd35708fb7b341319129a8aa10614c
Clemens Giuliani <clemens@inailuig.it>
2025-04-09 10:24:00 +0200

    update reduce xor

--- netket/jax/_lax.py
+++ netket/jax/_lax.py
@@ -4,6 +4,10 @@ import jax
 
 @partial(jax.jit, static_argnums=1)
 def reduce_xor(x, axes):
+    """
+    equivalent of jax.lax.reduce_xor,
+    with support for negative axes (counted from the back)
+    """
     if isinstance(axes, int):
         axes = (axes,)
     axes = tuple(i if i >= 0 else x.ndim + i for i in axes)
